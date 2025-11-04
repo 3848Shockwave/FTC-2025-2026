5ff7f84569892f1e67cef59bb463f7a7b761effb
tianruijia <tianruijia2008@gmail.com>
2025-11-03 18:45:16 -0800

    temp test for adding intake system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpProgram.java
@@ -4,6 +4,9 @@ import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
 
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import dev.nextftc.bindings.Button;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.extensions.pedro.PedroComponent;
@@ -12,16 +15,23 @@ import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.driving.DriverControlledCommand;
+import dev.nextftc.hardware.impl.MotorEx;
+
 import org.firstinspires.ftc.teamcode.Subsystems.Drive;
 import org.firstinspires.ftc.teamcode.Subsystems.Turret;
 import org.firstinspires.ftc.teamcode.Tests.AprilTagWebCam;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
+import static dev.nextftc.bindings.Bindings.button;
 import static dev.nextftc.extensions.pedro.PedroComponent.follower;//most important one to import
 @TeleOp(name = "TeleOp Program", group = "Production")
 public class TeleOpProgram extends NextFTCOpMode {
     private final Pose startPose = new Pose(28.5, 128, Math.toRadians(180)); // Start Pose of our robot.
+
+    MotorEx intake = new MotorEx("intake").brakeMode();
+
+
     public TeleOpProgram(){
         addComponents(
                 new SubsystemComponent(Drive.INSTANCE),
@@ -31,9 +41,39 @@ public class TeleOpProgram extends NextFTCOpMode {
                 );
     }
 
+
+    private boolean motorToggle = false;
+
+    @Override
+    public void onInit() {
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Button a_button = button(() -> gamepad1.a).whenBecomesTrue(() ->
+        {
+            motorToggle = !motorToggle;
+            if (motorToggle) {
+                intake.setPower(1);
+            } else {
+                intake.setPower(0);
+            }
+        });
+
+        Button b_button = button(() -> gamepad1.b).whenBecomesTrue(()->
+        {
+            motorToggle = !motorToggle;
+            if (motorToggle) {
+                intake.setPower(-1);
+            } else {
+                intake.setPower(0);
+            }
+        });
+    }
+
     @Override
     public void onStartButtonPressed(){
 
+
+//        intake.setPower(1);
         Drive driveSubsystem = Drive.INSTANCE;
         follower().startTeleopDrive();
         DriverControlledCommand driverControlled = new PedroDriverControlled(
